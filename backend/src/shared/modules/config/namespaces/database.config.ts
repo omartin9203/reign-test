49cf10f8dd4960d609e37e0b7c8cca207d0ae26a
Oscar <o.martin9203@gmail.com>
2021-05-18 01:44:48 -0400

    feat[BACKEND](Config): Adding Config module

---
+++ backend/src/shared/modules/config/namespaces/database.config.ts
@@ -0,0 +1,12 @@
+import { registerAs } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+
+export const databaseConfig = registerAs('database', () => ({
+  connectString: process.env.DATABASE_CONNECT_STRING,
+}));
+
+export const databaseSchema = {
+  DATABASE_CONNECT_STRING: Joi
+    .string()
+    .default('mongodb://localhost:27017/reign_test'),
+};
